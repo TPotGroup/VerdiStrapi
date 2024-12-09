874cc77e4a919c5a51b0b7e0aa0f16970f0f1765
jhoabran <69612177+jhoabran@users.noreply.github.com>
2024-12-09 13:31:54 -0400

    added the amenities, teamMembers and Contact tables

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 5001),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-  url: env('URL'),
-  proxy: true
+  url: env("URL", "http://localhost:5001"),
+  proxy: true,
 });
